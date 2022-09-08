81f9a0551897179b6c47b916b5320c15bd2b01cc
juliMAB <julian6aguirre@hotmail.com>
2022-09-08 17:33:11 -0300

    Added region to Taxi2.cs.

--- Assets/MySCRIPTS/Taxi2.cs
+++ Assets/MySCRIPTS/Taxi2.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class Taxi2 : MonoBehaviour
 {
-    [SerializeField] Vector3 DestinationPoint;
-
-    [SerializeField] Rigidbody _rb;
+    #region EXPOSED_FIELD
+    [SerializeField] Transform firstNode;
 
     [SerializeField] float speed;
+    #endregion
 
+    #region PRIVATE_FIELDS
+
+    private Rigidbody _rb;
+    
+    private Vector3 DestinationPoint;
+    #endregion
+
+    #region UNITY_CALLS
+
+    private void OnValidate()
+    {
+        DestinationPoint = firstNode.position;
+        if (_rb==null)
+            _rb.GetComponent<Rigidbody>();
+    }
     private void Start()
     {
         transform.LookAt(DestinationPoint, Vector3.up);
@@ -18,12 +33,14 @@ public class Taxi2 : MonoBehaviour
     {
         _rb.MovePosition(transform.position + transform.forward * Time.deltaTime * speed);
     }
+
+    #endregion
+
+    #region PUBLIC_METHODS
     public void SetDestination(Vector3 newDestination)
     {
         DestinationPoint = newDestination;
         transform.LookAt(DestinationPoint, Vector3.up);
     }
-
-
-
+    #endregion
 }
